c63a5c36c17c8a98ef0152d5faf761738e7bf50f
R00t-Shell <RootShell@r00t-shell.com>
2025-02-24 15:32:26 +0300

    Update 0xShell.php

--- 0xShell.php
+++ 0xShell.php
@@ -4,7 +4,7 @@
     |  Author: R00t Shell                                                             |
     |  Date: 2025-02-18 06:45:35                                                      |
     |  Website: https://r00t-shell.com                                                |
-    |  Virus Total: a5ae0aab352871bc0b038b3aa43b03eb223425628c884af7b9fc592cd34eb86c  |
+    |  Virus Total: a8facd93e135dab9085fac5239b5a0658a263b6d691ce375c459abaa94e59729  |
     |  Description: Obfuscates PHP code to increase security and protect source code. |
     |_________________________________________________________________________________|
 */
